a591bd331c5cde356c1a46a9e4c84785cdb204c5
Mikhail Komarov <nemo1369@gmail.com>
2017-01-12 04:17:51 +0300

    Apple code signature and bundle gui identifier made injectable for iOS 10.2

--- ios-10-2.cmake
+++ ios-10-2.cmake
@@ -2,9 +2,9 @@
 # All rights reserved.
 
 if(DEFINED POLLY_IOS_10_2_CMAKE_)
-  return()
+    return()
 else()
-  set(POLLY_IOS_10_2_CMAKE_ 1)
+    set(POLLY_IOS_10_2_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
@@ -14,20 +14,23 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 set(IOS_SDK_VERSION 10.2)
 set(POLLY_XCODE_COMPILER "clang")
 polly_init(
-    "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
+        "iOS ${IOS_SDK_VERSION} Universal (iphoneos + iphonesimulator) / \
 ${POLLY_XCODE_COMPILER} / \
 c++11 support"
-    "Xcode"
+        "Xcode"
 )
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
 
 include(polly_fatal_error)
 
+set(APPLE_BUNDLE_GUI_IDENTIFIER "" CACHE STRING "Bundle GUI identifier")
+set(APPLE_CODE_SIGN_IDENTITY "" CACHE STRING "Code sign identity")
+
 # Fix try_compile
-set(MACOSX_BUNDLE_GUI_IDENTIFIER com.example)
+set(MACOSX_BUNDLE_GUI_IDENTIFIER ${APPLE_BUNDLE_GUI_IDENTIFIER})
 set(CMAKE_MACOSX_BUNDLE YES)
-set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "iPhone Developer")
+set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY ${APPLE_CODE_SIGN_IDENTITY})
 
 set(IPHONEOS_ARCHS armv7;armv7s;arm64)
 set(IPHONESIMULATOR_ARCHS i386;x86_64)
